b9985a781f0f57814544c3373467cf8da7617aa7
valentin <valentin.lefranc@see-d.fr>
2019-12-10 09:17:12 +0100

    first commit by valentin

---
+++ RunAPP.R
@@ -0,0 +1,8 @@
+library('shiny')
+####### A changer en fonction de l'ordinateur ##############################
+#setwd("C:/Users/admin/Dropbox (Compass FT)/Coachee/R/dash") # Valentin Windows
+#setwd("C:/Users/Service Informatique/Dropbox/ProjetCaisse/dash") ## Windows
+setwd("~/Dropbox/Cours_R/ProjetCaisse/dash")  ## Valentin Ubuntu
+
+############################################################################
+runApp()
