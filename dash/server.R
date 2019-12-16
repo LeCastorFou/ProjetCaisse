314c9dda29db5582cd676f6f38df0da052fa7246
laviateur <58725194+laviateur@users.noreply.github.com>
2019-12-16 23:29:43 +0100

    Update server.R

--- dash/server.R
+++ dash/server.R
@@ -159,6 +159,7 @@ server = function(input, output,session) {
                                            
                                            print(df_expose)
                                            df <- datatable(df_expose, rownames = F,
+                                                           colnames = c('Prix' = 'V1', 'Montant soumis' = 'V2', 'Montant TVA' = 'V3', 'Montant Total' = 'V4'),
                                                           # colnames = c("Prix", "Mont.Soumis", "Mont.TVA", "Mont.Total", "Désignation.Famille"),
                                                            extension = "Buttons",
                                                            filter='none',
