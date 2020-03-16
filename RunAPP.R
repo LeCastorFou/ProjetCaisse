e99343335b19e58cdb115002e93b2a4d7be36fdb
laviateur <58725194+laviateur@users.noreply.github.com>
2020-03-16 17:18:25 +0100

    Delete RunAPP.R

--- RunAPP.R
+++
@@ -1,9 +0,0 @@
-library('shiny')
-####### A changer en fonction de l'ordinateur ##############################
-#setwd("C:/Users/admin/Dropbox (Compass FT)/Coachee/R/dash") # Valentin Windows
-setwd("~/R/ProjetCaisse/ProjetCaisse/dash") ## Windows
-#setwd("~/GitHub/R_caisse/dash") ## Valentin Ubuntu
-
-############################################################################
-runApp()
-
